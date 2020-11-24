2e2e731563816fd8b99ad4a2b1a1876ab57a0917
tbigot <tbigot@student.42.fr>
2020-11-24 14:17:31 +0100

    b

--- philo_one/srcs/fork.c
+++ philo_one/srcs/fork.c
@@ -6,7 +6,7 @@
 /*   By: tbigot <tbigot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/03 13:34:46 by tbigot            #+#    #+#             */
-/*   Updated: 2020/11/23 16:20:53 by tbigot           ###   ########.fr       */
+/*   Updated: 2020/11/24 14:17:21 by tbigot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 void		take_fork(t_sophos *sophos)
 {
 	pthread_mutex_lock(&g_mutex[sophos->fork1]);
-	sophos_activity(sophos->number, " has taken a fork\n", g_sophos_die);
+	sophos_activity(sophos->number, " has taken a fork\n", 1);
 	pthread_mutex_lock(&g_mutex[sophos->fork2]);
-	sophos_activity(sophos->number, " has taken a fork\n", g_sophos_die);
+	sophos_activity(sophos->number, " has taken a fork\n", 1);
 }
 
 void		put_fork(t_sophos *sophos)
